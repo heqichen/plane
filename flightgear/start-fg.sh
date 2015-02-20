0965b19902ee27c80bead8c56707669024600858
HE Qichen <heqichen@gmail.com>
2015-02-20 16:06:05 +0800

    update flightgear start command

--- flightgear/start-fg.sh
+++ flightgear/start-fg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fgfs  --generic=socket,out,1,127.0.0.1,8080,tcp,fg-protocol --generic=socket,in,1,127.0.0.1,8081,tcp,fg-protocol --httpd=8082
+fgfs  --generic=socket,out,10,127.0.0.1,8080,tcp,fg-protocol --generic=socket,in,10,127.0.0.1,8081,tcp,fg-protocol --httpd=8082
