4ce579a899eadb3ffbb32330aba70b00dc4d9a0b
HE Qichen <heqichen@gmail.com>
2015-03-03 20:48:21 +0000

    add altitude fusion

---
+++ chicken/altitude_fusion.h
@@ -0,0 +1,5 @@
+#ifndef __ALTITUDE_FUSION_H__
+#define __ALTITUDE_FUSION_H__
+
+#endif
+
--- chicken/chicken.cpp
+++ chicken/chicken.cpp
@@ -14,91 +14,33 @@
 
 using namespace std;
 
-void setupSystem(void);
+Io *io;
+DeviceManager *devManager;
+Radio *radio;
+Imu *imu;
+ServoController *sc;
+Aoa *aoa;
+Beeper *beeper;
+Agl *agl;
+
+void setupDevice(void);
 
 int main(int argc, char *argv[])
 {
-	setupSystem();
+	setupDevice();
 
 	return 0;
 }
 
-void setupSystem(void)
+void setupDevice(void)
 {
-	Io *io = new Io();
-	DeviceManager devManager(io);
-	Radio *radio = devManager.getRadio();
-	Imu *imu = devManager.getImu();
-	ServoController *sc = devManager.getServoController();
-	Aoa *aoa = devManager.getAoa();
-	Beeper *beeper = devManager.getBeeper();
-	Agl *agl = devManager.getAgl();
-
-	while (true)
-	{
-		/*
-		//test send attitude
-
-		usleep(100000UL);
-		ImuAttitude a = imu->getAttitude();
-		RawMagValue mag = imu->getRawMagValue();
-		cout<<"pitch: " << a.pitch<<"\troll: " << a.roll<<"\theading:"<<a.heading<<endl;
-		cout<<"magnetometer x: " << mag.x << "\ty: "<<mag.y<<"\tz: "<<mag.z<<endl;
-
-		uint8_t buf[MAVLINK_MAX_PACKET_LEN];
-		mavlink_message_t msg;
-		mavlink_system_t mavlink_system;
-		mavlink_system.sysid = 250;
-		//mavlink_system.compid = MAV_COMP_ID_ALL;     ///< The component sending the message is the IMU, it could be also a Linux process
-		mavlink_system.compid = 0;
-		mavlink_system.type = MAV_TYPE_FIXED_WING;   ///< This system is an airplane / fixed wing
-
-		mavlink_attitude_t attitude;
-		attitude.time_boot_ms = millis();
-		attitude.roll = a.roll;
-		attitude.pitch = a.pitch;
-		attitude.yaw = a.heading;
-		attitude.rollspeed = 0;
-		attitude.pitchspeed = 0;
-		attitude.yawspeed = 0;
-		mavlink_msg_attitude_encode(
-			mavlink_system.sysid,
-			MAV_COMP_ID_IMU,
-			&msg,
-			&attitude);
-		uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-		radio->write(buf, len);
-		*/
-
-		//test read servo
-		/*
-		usleep(20000UL);
-		ServoSignal rawRc = sc->getRawServoSignal();
-		if (rawRc.status > 1500)
-		{
-			sc->startWriteServoSignal();
-			ServoSignal overrideRc = rawRc;
-			sc->writeServoSignal(overrideRc);
-		}
-		else
-		{
-			sc->stopWriteServoSignal();
-		}
-
-		//cout<<"throttle: "<<rawRc.throttle<<endl;
-		*/
-
-		/*
-		usleep(500000UL);
-		cout<<"aoa: "<<aoa->readAoa()<<endl;
-		cout<<"side slip: "<<aoa->readSideSlip()<<endl;
-		*/
-
-		//beeper->test();
-
-		//test ir sensor
-		usleep(100000UL);
-		cout<<agl->measureAgl()<<endl;
-
-	}
+	io = new Io();
+	devManager = new DeviceManager(io);
+	
+	radio = devManager->getRadio();
+	imu = devManager->getImu();
+	sc = devManager->getServoController();
+	aoa = devManager->getAoa();
+	beeper = devManager->getBeeper();
+	agl = devManager->getAgl();
 }
