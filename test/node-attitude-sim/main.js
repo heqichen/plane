a4ba1ce95ef6f97f7bde7cc7b388f612a8e2bd7c
HE Qichen <heqichen@gmail.com>
2015-03-05 04:55:07 +0800

    output data in rad

--- test/node-attitude-sim/main.js
+++ test/node-attitude-sim/main.js
@@ -3,12 +3,16 @@ var SerialPort = require("serialport").SerialPort;
 var mavlink = require('mavlink');
 var net = require("net");
 
+var PI = 3.1415926535897932384626433832795;
+var DEG2RAD = PI/180.0;
+
+
 var myMav = new mavlink(250, 0, "v1.0",["common"]);
 
 var isMavlinkReady = false;
 var isSerialportReady = false;
 
-var flightGearCmd = "fgfs --generic=socket,out,10,127.0.0.1,8081,tcp,attitude-sim --httpd=8080"
+var flightGearCmd = "fgfs --generic=socket,out,20,127.0.0.1,8081,tcp,attitude-sim --httpd=8080"
 
 myMav.on("ready", function() {
 	console.log("mavlink ready");
@@ -19,7 +23,7 @@ myMav.on("ready", function() {
 			console.log("failed to open serial port: " + error);
 		}
 		console.log("serialport opened");
-		console.log(" please runn command");
+		console.log(" please run command");
 		console.log(flightGearCmd);
 
 		var tcpServer = net.createServer(function(conn) {
@@ -30,9 +34,9 @@ myMav.on("ready", function() {
 				var d = new Date();
 				myMav.createMessage("ATTITUDE", {
 					"time_boot_ms"	: d.getMilliseconds(), 
-					"roll"			: fgAttitude.roll, 
-					"pitch"			: fgAttitude.pitch,
-					"yaw"			: fgAttitude.heading,
+					"roll"			: fgAttitude.roll * DEG2RAD,
+					"pitch"			: fgAttitude.pitch * DEG2RAD,
+					"yaw"			: fgAttitude.heading * DEG2RAD,
 					"rollspeed"		: 0,
 					"pitchspeed"	: 0,
 					"yawspeed"		: 0,
